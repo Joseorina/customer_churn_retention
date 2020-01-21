3250f2ce01cc218263b411d917842b9b668caa8b
Joseorina <orinangare@gmail.com>
2020-01-21 09:53:05 +0300

    [flask predict]

---
+++ flask_predict_model.py
@@ -0,0 +1,70 @@
+# Artificial neural network
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# importing teh dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:,13].values
+
+# encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+labelencoder_X_1 = LabelEncoder()
+X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
+
+labelencoder_X_2 = LabelEncoder()
+X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
+
+onehotencoder = OneHotEncoder(categorical_features=[1])
+X = onehotencoder.fit_transform(X).toarray()
+X = X[:, 1:]
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+
+## MAking the ANN
+from tensorflow.contrib.keras.api.keras.models import Sequential
+from tensorflow.contrib.keras.api.keras.layers import Dense
+from tensorflow.contrib.keras import backend
+
+# Initialising the ANN
+classifier = Sequential()
+
+# Adding the input layer and the first hidden layer
+classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu', input_dim=11))
+
+# Adding the second hidden layer
+classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu'))
+
+# Adding the output layer
+classifier.add(Dense(units=1, kernel_initializer='uniform', activation='sigmoid'))
+
+# Compiling the ANN
+classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Fitting the ANN to the Training set
+classifier.fit(X_train, y_train, batch_size=10, epochs=100, validation_split=0.1)
+
+## Part 3 Mkaing predictions and evaluating the model
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+## y_pred = (y_pred > 0.5)
+
+import pickle
+with open('/home/trolaf/Desktop/projects/customer_churn_retention/srf.pkl', 'wb') as model_pkl:
+    pickle.dump(classifier, model_pkl, protocol=2)
+        
